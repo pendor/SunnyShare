1b82d6c9e106b10328638c9d4c712faa77528692
Zachary Bedell <zac@thebedells.org>
2016-05-08 19:17:02 -0400

    Add button to reassociate wlan1 from admin page

--- www/admin/net.php
+++ www/admin/net.php
@@ -3,11 +3,37 @@ require_once('../config.php');
 require_once('../functions.php');
 
 checkAdmin();
-  
+
+if(isset($_GET['r']) && $_GET['r'] == '1') {
+  $_SESSION['rd'] = '1';
+  header('Location: /admin/net.php');
+  exit;
+} 
+
 printHeader(true);
-?>
-  <h1>Network Info:</h1>
+
+if(isset($_SESSION['rd']) && $_SESSION['rd'] == '1') {
+  unset($_SESSION['rd']);
+
+  echo '<h2>Reassociating WiFi...</h2>';
   
+  $out = `/usr/bin/sudo /sbin/ifdown --verbose wlan1 2>&1`;
+  echo '<pre>' . $out . '</pre>';
+  
+  sleep(2);
+  
+  $out = `/usr/bin/sudo /sbin/ifup --verbose wlan1 2>&1`;
+  echo '<pre>' . $out . '</pre>';
+  
+  sleep(2);
+  
+  echo '<span>Refresh in a few seconds to check DHCP results.</span><br/>';
+} else {
+  echo '<a class="delbtn" href="?r=1">Reassociate WiFi</a>';
+}
+?>
+  
+  <h1>Network Info:</h1>
   <b>Client MAC:</b> <?= getClientMac() ?><br/>
   
   <h2>Associated WiFi</h2>
