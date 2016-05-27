fcfb1640ee3fb664c8dba8d53043d05a15604f8d
Zachary Bedell <zac@thebedells.org>
2016-05-26 22:02:13 -0400

    Don't watchdog if up for less than 10 minutes

--- etc/bin/wd-test.sh
+++ etc/bin/wd-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ifconfig wlan1 || exit 1
+if [ `cut -d. -f1 < /proc/uptime` -lt 600 ] ; then 
+  echo "System not up long enough to watchdog yet."
+  exit 0
+fi
 
 if [ ! -b /dev/sda ] ; then
 	exit 1
