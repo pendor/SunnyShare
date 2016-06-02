aaa5f7f274341c6a77f66469cd4a73df1bf0a2bd
Zachary Bedell <zac@thebedells.org>
2016-06-02 19:03:06 -0400

    Set cookie to keep about hidden on home page

--- www/index.php
+++ www/index.php
@@ -3,19 +3,20 @@ require_once('functions.php');
 require_once('shout.php'); 
 printHeader();
 ?>
-    <div class="box">
-      <a href="#" onclick="$(this).parent().hide(1000);" class="boxclose">X</a>
-    <h1>What's This?!?</h1>
+    <div class="box" id="aboutbox">
+      <a href="#" onclick="document.cookie = 'boxclosed=1'; $(this).parent().hide(1000);" class="boxclose">X</a>
+      <h1>What's This?!?</h1>
     
-    <p>You've discovered a Sunny+Share box placed near-by by someone who
-      values sharing and collaboration in the local area.  You may use
-      this service to leave messages for others who are near by and share files.
-      This site is <b>not</b> available on the internet.  You can access it only
-      when near by &amp; connected to the Sunny+Share WiFi connection.
-    </p>
+      <p>You've discovered a Sunny+Share box placed near-by by someone who
+        values sharing and collaboration in the local area.  You may use
+        this service to leave messages for others who are near by and share files.
+        This site is <b>not</b> available on the internet.  You can access it only
+        when near by &amp; connected to the Sunny+Share WiFi connection.
+      </p>
           
-    <p>For more information about this service, see <a href="/about.php">About Sunny+Share</a></p>
+      <p>For more information about this service, see <a href="/about.php">About Sunny+Share</a></p>
 	  </div>
+    
 		<div id="shoutbox">
 		  <h2>Announcement board: Leave messages for near-by wanderers</h2>
 			<noscript><h2>Sorry, but chat won't work without JavaScript enabled.</h2></noscript>
@@ -24,4 +25,9 @@ printHeader();
       drawChatBox();
       ?>
 		</div>
+    <script type="text/javascript">
+    if(document.cookie.indexOf('boxclosed') >= 0) {
+      $('#aboutbox').hide();
+    }
+    </script>
 <?php printFooter(); ?>
