6e400d99fde53853962885fb1f73791ec19fd8b7
Zachary Bedell <zac@thebedells.org>
2016-06-04 01:04:43 -0400

    Apply settings unconditionally

--- etc/bin/config-axp20x.sh
+++ etc/bin/config-axp20x.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
 if [ -d /sys/power/axp_pmu ] || [ -e /sys/bus/i2c/drivers/axp20x/0-0034 ] ; then
-
   # Enable all voltage / current monitors if not already.
-  if [ `i2cget -y -f 0 0x34 0x82` != "0xff" ] ; then
-  	i2cset -y -f 0 0x34 0x82 0xff
-  fi
+ 	i2cset -y -f 0 0x34 0x82 0xff
   
   # Set longer delays on power button
-  if [ `i2cget -y -f 0 0x34 0x36` != "0xff" ] ; then
-  	i2cset -y -f 0 0x34 0x36 0xff
+ 	i2cset -y -f 0 0x34 0x36 0xff
     
-    # Make sure the PEK registers start out at 0
-    i2cset -y -f 0 0x34 0x4a 0x03
+  # Make sure the PEK registers start out at 0
+  i2cset -y -f 0 0x34 0x4a 0x03
     
-    # 1200mA max charge current
-    # i2cset -y -f 0 0x34 0x33 0xc9
-    
-    # 1800mA max charge current
-    i2cset -y -f 0 0x34 0x33 0xcf
-  fi
-  
+  # 1200mA max charge current
+  # i2cset -y -f 0 0x34 0x33 0xc9
+   
+  # 1800mA max charge current
+  i2cset -y -f 0 0x34 0x33 0xcf
 fi
 
 echo "none" > /sys/class/leds/bananapro\:green\:usr/trigger
